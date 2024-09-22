038f45a5f9dfd61abfbe961abcfba86727c08a6f
Phan Nguyen Ngoc Hien <31280143+RoboMechatronics@users.noreply.github.com>
2024-09-22 16:34:25 +0700

    Add files via upload
    Initial

---
+++ Folder Panel.Designer.cs
@@ -0,0 +1,243 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class FolderPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Open1 = new System.Windows.Forms.Button();
+            this.Open2 = new System.Windows.Forms.Button();
+            this.Open3 = new System.Windows.Forms.Button();
+            this.Open4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.ProjectLocation = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(267, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag and drop to image";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Open1
+            // 
+            this.Open1.BackColor = System.Drawing.SystemColors.Control;
+            this.Open1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Open1.FlatAppearance.BorderSize = 0;
+            this.Open1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open1.Location = new System.Drawing.Point(217, 113);
+            this.Open1.Name = "Open1";
+            this.Open1.Size = new System.Drawing.Size(84, 32);
+            this.Open1.TabIndex = 1;
+            this.Open1.Text = "Open";
+            this.Open1.UseVisualStyleBackColor = true;
+            // 
+            // Open2
+            // 
+            this.Open2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open2.Location = new System.Drawing.Point(477, 204);
+            this.Open2.Name = "Open2";
+            this.Open2.Size = new System.Drawing.Size(84, 32);
+            this.Open2.TabIndex = 1;
+            this.Open2.Text = "Open";
+            this.Open2.UseVisualStyleBackColor = true;
+            // 
+            // Open3
+            // 
+            this.Open3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open3.Location = new System.Drawing.Point(217, 204);
+            this.Open3.Name = "Open3";
+            this.Open3.Size = new System.Drawing.Size(84, 32);
+            this.Open3.TabIndex = 1;
+            this.Open3.Text = "Open";
+            this.Open3.UseVisualStyleBackColor = true;
+            // 
+            // Open4
+            // 
+            this.Open4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open4.Location = new System.Drawing.Point(477, 113);
+            this.Open4.Name = "Open4";
+            this.Open4.Size = new System.Drawing.Size(84, 32);
+            this.Open4.TabIndex = 1;
+            this.Open4.Text = "Open";
+            this.Open4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.documents;
+            this.pictureBox1.Location = new System.Drawing.Point(126, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.qa;
+            this.pictureBox2.Location = new System.Drawing.Point(372, 96);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(99, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.send_message;
+            this.pictureBox3.Location = new System.Drawing.Point(126, 184);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.icons8_solidworks_512;
+            this.pictureBox4.Location = new System.Drawing.Point(372, 184);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(99, 65);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // ProjectLocation
+            // 
+            this.ProjectLocation.BackColor = System.Drawing.SystemColors.Control;
+            this.ProjectLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ProjectLocation.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ProjectLocation.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProjectLocation.ForeColor = System.Drawing.Color.Black;
+            this.ProjectLocation.Location = new System.Drawing.Point(124, 30);
+            this.ProjectLocation.Name = "ProjectLocation";
+            this.ProjectLocation.Size = new System.Drawing.Size(441, 26);
+            this.ProjectLocation.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Project Location:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(124, 292);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(441, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Progress:";
+            // 
+            // FolderPanel
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(607, 338);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ProjectLocation);
+            this.Controls.Add(this.Open4);
+            this.Controls.Add(this.Open3);
+            this.Controls.Add(this.Open1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Open2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FolderPanel";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Folder Panel";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Open1;
+        private System.Windows.Forms.Button Open2;
+        private System.Windows.Forms.Button Open3;
+        private System.Windows.Forms.Button Open4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private TextBox ProjectLocation;
+        private Label label2;
+        private ProgressBar progressBar1;
+        private Label label3;
+    }
+}
+
